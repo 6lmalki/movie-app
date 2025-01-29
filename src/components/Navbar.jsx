22390f6fb036aeefef17c739ba4f43c07cd3bbbe
6lmalki <mahmood.6lmalki@gmail.com>
2025-01-29 02:03:37 -0500

    Added Movie App project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "../css/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">Movie App</Link>
+      </div>
+      <div className="navbar-links">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="/favorites">
+          Favorites
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
